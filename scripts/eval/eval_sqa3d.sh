cd6e04a14dc212481da078fcd438a3eb0380dc37
Hongbo-Jin <Eter1118@163.com>
2025-06-05 16:36:28 +0800

    jhb

--- scripts/eval/eval_sqa3d.sh
+++ scripts/eval/eval_sqa3d.sh
@@ -7,4 +7,4 @@ python hbllava/eval/eval_sqa3d.py \
     --num-frame ${NUM_frame} \
     --gt-file /mnt/cloud_disk/jhb/binjiang/HBLLaVA/data/gt_files/sqa3d/gt_test_processed.json  \
     --data-folder /mnt/cloud_disk/public_data/ScanNet_for_ScanQA_SQA3D/downsample_32_w_3d_features/posed_images/ \
-    --answer-file /mnt/cloud_disk/jhb/binjiang/HBLLaVA/output/${NUM_frame}_${model_name}_sqa3d.json
+    --answer-file /mnt/cloud_disk/jhb/binjiang/HBLLaVA/output/sqa3d/${NUM_frame}_${model_name}.json
